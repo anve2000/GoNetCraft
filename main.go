7daa5c6d24f35f346f8f2dea009f4812c9d921fb
anvesha <anvesha@binmile.com>
2025-07-17 23:46:52 +0530

    passing byte slice into Read()

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main(){
 	resp, err:=http.Get("http://google.com");
 	if(err!=nil){
 		fmt.Println("err ", err);
+		os.Exit(1)
 	}
+
+	bs:=make([]byte, 99999);
+	resp.Body.Read(bs);
+	fmt.Println(string(bs));
 	fmt.Println("Resp ", resp);
 }
